6ee5d18766ad12198228ecd1745877fcaa87efe6
Mohamed Adel <mohamedadelsa4@gmail.com>
2024-10-23 00:55:17 +0300

    feat: footer

--- src/app/layouts/main-layout/main-layout.component.ts
+++ src/app/layouts/main-layout/main-layout.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TopBarComponent } from '../../components/top-bar/top-bar.component';
 
 @Component({
   selector: 'app-main-layout',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TopBarComponent],
   templateUrl: './main-layout.component.html',
 })
 export default class MainLayoutComponent {}
